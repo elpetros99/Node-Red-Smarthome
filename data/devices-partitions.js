8a449af897dc7607426629f26060b89ac0375924
IamTheo2000 <93789269+IamTheo2000@users.noreply.github.com>
2023-03-01 22:49:34 +0200

    Data  creation

---
+++ data/devices-partitions.js
@@ -0,0 +1,12 @@
+const partitionsPerDevice = {
+    TH: 0,
+    HVAC: 1,
+    MiAC: 2,
+    W: 3,
+    Etot: 4,
+    Wtot: 5,
+    Mov: 6,
+    alarm: 7,
+};
+
+module.exports = { partitionsPerDevice };
